c9582d8e5bea19a36d265e6d0c487677161ca81f
joexy <josephethethowo@gmail.com>
2023-10-10 16:26:49 +0100

    create sign in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,44 @@
 import User from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
+import errorHandler from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
    try {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    await newUser.save()
-    res.status(201).json({
-        msg: "User created successfully",
-        newUser
-    })
+        const { username, email, password } = req.body;
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save()
+        res.status(201).json({
+            msg: "User created successfully",
+            newUser
+        })
    } catch (error) {
          next(error)
    }
+};
+
+export const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Wrong credentials"));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        const { password: userPassword, ...user } = validUser._doc;
+        res
+            .cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json({
+                msg: "User logged in successfully",
+                user
+        });
+
+    } catch (error) {
+        next(error)
+    }
 };
