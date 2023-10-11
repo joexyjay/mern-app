abb781261061d741cf94e3d74f92ca565dcc65dc
joexy <josephethethowo@gmail.com>
2023-10-11 10:28:56 +0100

    complete signin page functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -28,10 +28,11 @@ export const signin = async (req, res, next) => {
         if (!validPassword) {
             return next(errorHandler(401, "Wrong credentials"));
         }
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: userPassword, ...user } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 60 * 60 * 1000 * 24)
         res
-            .cookie("access_token", token, { httpOnly: true })
+            .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
             .status(200)
             .json({
                 msg: "User logged in successfully",
