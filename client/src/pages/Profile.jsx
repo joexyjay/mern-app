fb0835fa50341b6afc467a638091a5dba38db567
joexy <josephethethowo@gmail.com>
2023-10-30 10:25:43 +0100

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,13 @@ import { useRef, useState, useEffect } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import { app } from "../firebase"
 import { useDispatch } from "react-redux"
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice"
+import { 
+  updateUserStart, 
+  updateUserSuccess, 
+  updateUserFailure, 
+  deleteUserStart, 
+  deleteUserSuccess, 
+  deleteUserFailure } from "../redux/user/userSlice"
 export default function Profile() {
   const dispatch = useDispatch()
   const fileRef = useRef(null)
@@ -77,6 +83,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error))
     }
   }
+
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser?.user?._id}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error))
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Profile</h1>
@@ -134,7 +157,7 @@ export default function Profile() {
           hover:opacity-95 disabled:opacity-80">{loading? 'Loading...' : 'Update'}</button>
       </form>
       <div className="flex justify-between mt-4">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
